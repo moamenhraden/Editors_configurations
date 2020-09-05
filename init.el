b92efbc0f6d0d267fc3c2872266ae465e8cf2ce2
moamnehraden <momenhredien@gmail.com>
2020-09-05 05:34:28 +0200

    i dont know

--- init.el
+++ init.el
@@ -19,7 +19,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (org-bullets evil doom-modeline prettier rjsx-mode nyan-mode))))
+    (git-gutter-fringe git-timemachine org-bullets evil doom-modeline prettier rjsx-mode nyan-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
